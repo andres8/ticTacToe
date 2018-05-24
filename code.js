0cbeac0bd39a4dc742276650bcd6c7f472526dee
Andres Diaz <mrdayz8@gmail.com>
2018-05-24 20:55:37 +0200

    Improved AI

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 var playerToken = 'X';
 var computerToken = 'O';
-var player = 0;
+var jugador = 0;
 var turn;
 var score1 =0;
 var score2 =0;
@@ -12,54 +12,109 @@ $(document).ready(function(){
     ['','','']
   ];
   //check if the game is over
-  function gameIsOver(){
+  function gameIsOver(gridv){
       // check horizontal
       for (var i=0; i<3; i++){
-        if(grid[i][0] !== '' &&
-        grid[i][0] === grid[i][1] &&
-        grid[i][1] === grid[i][2]){
-        return grid[i][0];
+        if(gridv[i][0] !== '' &&
+        gridv[i][0] === gridv[i][1] &&
+        gridv[i][1] === gridv[i][2]){
+        return gridv[i][0];
         }
       }
       // check vertical
       for (var j=0; j<3; j++){
-        if(grid[0][j] !== '' &&
-        grid[0][j] === grid[1][j] &&
-        grid[1][j] === grid[2][j]){
-        return grid[0][j];
+        if(gridv[0][j] !== '' &&
+        gridv[0][j] === gridv[1][j] &&
+        gridv[1][j] === gridv[2][j]){
+        return gridv[0][j];
         }
       }
       // check diagonal
-      if(grid[0][0] !== '' &&
-        grid[0][0] === grid[1][1] &&
-        grid[1][1] === grid[2][2]){
-        return grid[0][0];
+      if(gridv[0][0] !== '' &&
+        gridv[0][0] === gridv[1][1] &&
+        gridv[1][1] === gridv[2][2]){
+        return gridv[0][0];
       }
       // check diagonal
-      if(grid[2][0] !== '' &&
-        grid[2][0] === grid[1][1] &&
-        grid[1][1] === grid[0][2]){
-        return grid[2][0];
+      if(gridv[2][0] !== '' &&
+        gridv[2][0] === gridv[1][1] &&
+        gridv[1][1] === gridv[0][2]){
+        return gridv[2][0];
       }
       for(var i=0; i<3; i++){
         for (var j=0; j<3; j++){
-          if(grid[i][j]=== ''){
+          if(gridv[i][j]=== ''){
             return false;
           }
         }
       }
       return null;
   }
-  //Computer movement
-  function moveAi(){
-    for(var i=0; i<3; i++){
-      for(var j=0 ; j<3; j++){
-        if(grid[i][j]== ''){
-          return {i:i,
-                  j:j}
+  function minmax(newGrid, depth, player){
+    const gameState = gameIsOver(newGrid);
+    if(gameState === false){
+      const values = [];
+      for(var i=0; i<3; i++){
+        for (var j=0; j<3; j++){
+          const gridCopy = _.cloneDeep(newGrid);
+          //const gridCopy = JSON.parse(JSON.stringify(newGrid));
+          if(gridCopy[i][j]!== '') continue;
+          gridCopy[i][j] = player;
+          const value = minmax(gridCopy, depth + 1, (player === playerToken) ? computerToken : playerToken);
+          values.push({
+            cost: value,
+            cell:{
+              i: i,
+              j: j
+            }
+          });
         }
       }
+      if(player === computerToken){
+        const max = _.maxBy(values, (v) =>{
+        return v.cost;
+        });
+        //function max (n){values.reduce(function(a, b) { return a.n >= b.n ? a : b }, {});}
+        if (depth===0){
+          //console.log(max.cell);
+        return max.cell;
+        // return max(values[0].cell);
+        } else {
+        return max.cost;
+        //return max(values[0].cost);
+        }
+      } else {
+        const min = _.minBy(values, (v) =>{
+        return v.cost;
+        });
+       //function min(n){values.reduce(function(a, b) { return a.n <= b.n ? a : b }, {});}
+        if (depth===0){
+         return min.cell;
+        //return min(values[0].cell);
+        } else {
+          return min.cost;
+        //return min(values[0].cost);
+        }
+      }
+    } else if (gameState === null){
+      return 0;
+    } else if (gameState === playerToken){
+      return depth - 10;
+    } else if (gameState === computerToken){
+      return 10 - depth;
     }
+  }
+  //Computer movement
+  function moveAi(){
+    return minmax(grid, 0, computerToken);
+    //for(var i=0; i<3; i++){
+      //for(var j=0 ; j<3; j++){
+        //if(grid[i][j]== ''){
+          //return {i:i,
+            //      j:j}
+        //}
+      //}
+    //}
   }
   //Restart Grid
   function ereaseGrid() {
@@ -72,7 +127,7 @@ $(document).ready(function(){
   }
   //Marker Points
   function marcScore(){
-    if(gameIsOver()==='X'){
+    if(gameIsOver(grid)==='X'){
       score1 += 1;
       $('.score1 .points').html(score1);
     } else{
@@ -82,18 +137,18 @@ $(document).ready(function(){
   }
   //show game result
   function showResult(){
-    var gameState = gameIsOver();
-    if(gameState && player==1 && turn==1){
+    var gameState = gameIsOver(grid);
+    if(gameState && jugador==1 && turn==1){
       $('.textResult').html("You Won!! :D")
       $('.result').fadeIn(400).delay(800).fadeOut(400);
-    }else if(gameState && player==1 && turn==2){
+    }else if(gameState && jugador==1 && turn==2){
       $('.textResult').html("Uh ho, you lost...")
       $('.result').fadeIn(400).delay(800).fadeOut(400);
-    }else if(gameState && player==2 && turn=='o'){
+    }else if(gameState && jugador==2 && turn=='o'){
       $('.textResult').html("Player X wins!! :D...")
       $('.result').fadeIn(400).delay(800).fadeOut(400);
     }
-    else if(gameState && player==2 && turn=='x'){
+    else if(gameState && jugador==2 && turn=='x'){
       $('.textResult').html("Player O wins!! :D...")
       $('.result').fadeIn(400).delay(800).fadeOut(400);
     }
@@ -105,13 +160,13 @@ $(document).ready(function(){
   }
   $('#onePlayer').click(function(){
     ereaseGrid();
-    player = 1;
+    jugador = 1;
     $('#firstScreen').fadeOut(400);
     $('#secondScreen').delay(400).fadeIn(500);
   });
   $('#twoPlayer').click(function(){
     ereaseGrid();
-    player = 2;
+    jugador = 2;
     $('#firstScreen').fadeOut(400);
     $('#thirdScreen').delay(400).fadeIn(500);
   });
@@ -161,7 +216,7 @@ $(document).ready(function(){
   });
   $('.col').click(function(){
     if(grid[$(this).data('i')][$(this).data('j')]==''){
-      if(player==1){
+      if(jugador==1){
         $(this).html(playerToken);
         var i = $(this).data('i');
         var j = $(this).data('j');
@@ -170,7 +225,7 @@ $(document).ready(function(){
         $('.turnPlayerX').animate({'top':'3px'},400);
         turn = 1;
         showResult();
-        if (gameIsOver()){
+        if (gameIsOver(grid)){
           $('.turnPlayerX').animate({'top':'-38px'},400);
           $('.turnPlayerO').animate({'top':'3px'},400);
           marcScore();
@@ -178,14 +233,14 @@ $(document).ready(function(){
           console.log(grid);
         } else{
             const move = moveAi();
-            setTimeout(function(){$('.col[data-i='+move.i+'][data-j='+move.j+']').html(computerToken)
+            setTimeout(function(){$('.col[data-i='+move.i+'][data-j='+move.j+']').html(computerToken)},200);
             $('.turnPlayerX').animate({'top':'-38px'},400);
-            $('.turnPlayerO').animate({'top':'3px'},400);},800);
+            $('.turnPlayerO').animate({'top':'3px'},400);
             grid[move.i][move.j] = computerToken;
             turn =2;
             setTimeout(function(){
               showResult();
-              if (gameIsOver()){
+              if (gameIsOver(grid)){
               marcScore();
               ereaseGrid();
               }
@@ -203,7 +258,7 @@ $(document).ready(function(){
           turn='o';
           setTimeout(function(){
             showResult();
-            if(gameIsOver()){
+            if(gameIsOver(grid)){
             marcScore();
             ereaseGrid();
             }
@@ -218,7 +273,7 @@ $(document).ready(function(){
           turn='x';
           setTimeout(function(){
             showResult();
-            if(gameIsOver()){
+            if(gameIsOver(grid)){
             marcScore();
             ereaseGrid();
             }
